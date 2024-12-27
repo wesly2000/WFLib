827afccfba12de256ad9d084d374afa9a3eceed0
wesly2000 <ylx.seu@foxmail.com>
2024-12-27 16:29:28 +0800

    Introduce Extractor

--- WFlib/tools/formatter.py
+++ WFlib/tools/formatter.py
@@ -1,4 +1,38 @@
 import numpy as np
+import pyshark
+
+class Extractor(object):
+    """
+    The class provides methods for the actual feature extraction work. This is some abstract class, and the 
+    extractors used MUST inherit it.
+    """
+    def __init__(self, name):
+        self.name = name
+
+    def extract(self):
+        raise NotImplementedError
+
+
+class DirectionExtractor(Extractor):
+    """
+    The class provides methods for the packet direction extraction.
+
+    Attributes
+    ----------
+    src : str
+        The source IP address for the extractor to decide ingress or egress.
+    """
+    def __init__(self, src, name=None):
+        self.name = "direction" if name is None else name 
+        self.src = src 
+
+    def extract(self, pkt):
+        src = pkt['ip'].src
+        if src == self.src: # egress packet
+            return 1
+        else:               # ingress packet
+            return -1
+
 
 class Formatter(object):
     """
@@ -15,42 +49,32 @@ class Formatter(object):
         """
         Attributes
         ----------
-        origin_buf : Any
+        raw_buf : Any
             The buffer to hold the loaded data in raw form.
 
         buf : dict:
             The buffer to hold the dict data, which should be the form like {lable_1: data_1, lable_2: data_2, ...},
             where label_X is a string, and data_X should be a np.ndarray.
         """
-        self.origin_buf = None
-        self.buf = None 
+        self.raw_buf = None
+        self.buf = dict()
 
-    def load(self, file) -> Exception:
+    def load(self, file):
         """
-        The load method that read in files specified by path into origin_buf.
+        The load method that read in files specified by path into raw_buf.
 
         Params
         ------
         file : file|str
             The file path to be read.
-
-        Returns
-        -------
-        Exception
-            To indicate the error in reading IO, None if no error occurs.
         """
-        return NotImplementedError
+        raise NotImplementedError
     
-    def transform(self) -> Exception:
+    def transform(self):
         """
-        Transform the origin_buf to buf, which is the operation to transform any data into dict form.
-
-        Returns
-        -------
-        Exception
-            To indicate the error in reading IO, None if no error occurs.
+        Transform the raw_buf to buf, which is the operation to transform any data into dict form.
         """
-        return NotImplementedError
+        raise NotImplementedError
 
     def dump(self, file):
         """
@@ -60,6 +84,61 @@ class Formatter(object):
         ------
         file : file|str
             The file path to be read.
-
         """
         np.savez()
+
+class PcapFormatter(Formatter):
+    def __init__(self):
+        """
+        Attributes
+        ----------
+        labels : list 
+            The labels for the files used. Each file corresponds to one label.
+
+        hosts : list
+            The hostnames of the requested websites. The index of a host corresponds to the the value of the label.
+
+        For example, for each of the hosts in [www.baidu.com, www.google.com, www.zhihu.com], we capture 3 request 
+        traces (.pcap). Then the labels after performing transform should be [0, 0, 0, 1, 1, 1, 2, 2, 2].
+        """
+        super().__init__()
+        self.labels = []
+        self.hosts = []
+
+    def load(self, file):
+        self.raw_buf = pyshark.FileCapture(input_file=file)
+
+    def transform(self, host : str, label : int, *extractors):
+        """
+        The transform method to extract features from self.raw_buf using the extractors. 
+        
+        NOTE: Ideally, the host and label parameters could be inferred from the filename. 
+        However, the inference is considered not the duty of the formatter itself but that of
+        the caller.
+
+        Params
+        ------
+        host : str
+            The hostname of the requested website.
+
+        label : int
+            The label assigned to the features extracted from the file.
+
+        extractors : list of Extractor
+            The extractors each of which extracts the feature named after Extractor.name, note that 
+            ONLY packet-level extractors should be passed. Burst/Flow-level or higher level extractors
+            should be used as extended methods.
+        """
+        if host not in self.hosts:
+            self.hosts.append(host)
+
+        self.labels.append(label)
+
+        for extractor in extractors:
+            # Initialize a new list for the given feature name
+            if extractor.name not in self.buf:
+                self.buf[extractor.name] = []
+        
+        for pkt in self.raw_buf:
+            for extractor in extractors:
+                self.buf[extractor.name].append(extractor.extract(pkt))
