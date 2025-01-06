1087468957bc4dfe0bf8a90b371f206d6022a29a
wesly2000 <ylx.seu@foxmail.com>
2025-01-06 17:02:15 +0800

    Add dry run mode

--- exp/data_capture/batch_capture.py
+++ exp/data_capture/batch_capture.py
@@ -4,6 +4,7 @@ This file is used to test batch_capture. Also, it could be used as a simple scri
 
 from WFlib.tools.capture import batch_capture, read_host_list
 import argparse
+import os
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
@@ -13,8 +14,20 @@ if __name__ == '__main__':
     parser.add_argument('-i', '--iface', type=str, help="The interface to perform capturing")
     parser.add_argument('-r', '--repeat', type=int, default=20, help="How many times the request will repeat")
     parser.add_argument('-t', '--timeout', type=int, default=200, help="The living time for a browsing session")
+    parser.add_argument('--dry-run', action='store_true', help="To output the file names will be created without actual creation.")
     args = parser.parse_args()
 
+    ssl_keylog_file = os.path.join(args.dir, "keylog.txt")
     host_list = read_host_list(args.list)
 
-    batch_capture(base_dir=args.dir, host_list=host_list, iface=args.iface, repeat=args.repeat, timeout=args.timeout)
+    if args.dry_run:
+        print("Dry run mode, the file names are listed below:")
+        for i in range(args.repeat):
+            for host in host_list:
+                host = host.strip()
+                output_file = os.path.join(args.dir, host, "{}_{:02d}.pcapng".format(host, i))
+                print(output_file)
+        print(f"{ssl_keylog_file}")
+    else:
+        os.environ["SSLKEYLOGFILE"] = ssl_keylog_file
+        batch_capture(base_dir=args.dir, host_list=host_list, iface=args.iface, repeat=args.repeat, timeout=args.timeout)
--- exp/data_capture/simple_capture.py
+++ exp/data_capture/simple_capture.py
@@ -4,6 +4,7 @@ This file is a test on using Selenium for automated website request
 from WFlib.tools.capture import capture
 import argparse
 import os
+from pathlib import Path
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
@@ -13,14 +14,26 @@ if __name__ == '__main__':
     parser.add_argument('-i', '--iface', type=str, help="The interface to perform capturing.")
     parser.add_argument('-r', '--repeat', type=int, default=1, help="How many times the request will repeat, if set with <=0, 1 will be used.")
     parser.add_argument('-t', '--timeout', type=int, default=200, help="The living time for a browsing session.")
+    parser.add_argument('--dry-run', action='store_true', help="To output the file names will be created without actual creation.")
     args = parser.parse_args()
 
     repeat = 1 if args.repeat < 1 else args.repeat
     timeout = 10 if args.timeout < 1 else args.timeout
 
+    ssl_keylog_file = os.path.join(args.dir, "keylog.txt")
     host = str.split(args.url, "//")[1] # Only fetch the hostname
 
-    for i in range(repeat):
-        output_file = os.path.join(args.dir, "{}_{:02d}.pcapng".format(host, i))
-        # Create and start the threads
-        capture(url=args.url, timeout=timeout, iface=args.iface, output_file=output_file)
+    if args.dry_run:
+        print("Dry run mode, the file names are listed below:")
+        for i in range(repeat):
+            output_file = os.path.join(args.dir, "{}_{:02d}.pcapng".format(host, i))
+            print(f"{output_file}")
+        print(f"{ssl_keylog_file}")
+    else:
+        os.environ["SSLKEYLOGFILE"] = ssl_keylog_file
+        for i in range(repeat):
+            # Set SSLKEYLOGFILE variable
+            Path("{}/{}".format(args.dir, host)).mkdir(parents=True, exist_ok=True)
+            output_file = os.path.join(args.dir, "{}_{:02d}.pcapng".format(host, i))
+            # Create and start the threads
+            capture(url=args.url, timeout=timeout, iface=args.iface, output_file=output_file)
