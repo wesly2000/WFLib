27630f29455591304b2ab22a082cb187627635c0
wesly2000 <ylx.seu@foxmail.com>
2025-01-22 14:18:54 +0800

    Filter out radan HTTP (8088)

--- WFlib/tools/capture.py
+++ WFlib/tools/capture.py
@@ -44,12 +44,12 @@ the capture.
 
 The semantics of the filter is that we ONLY want TCP or UDP packets, but the following protocols are NOT considered:
 
-LLMNR (5355), MDNS (5353), SOAP (3702), NTP (123), SSDP (1900), SSH (22), RDP (3389), DOT (853), HTTP (80)
+LLMNR (5355), MDNS (5353), SOAP (3702), NTP (123), SSDP (1900), SSH (22), RDP (3389), DOT (853), HTTP (80), Radan HTTP (8088)
 
 NOTE: This filter is not exhausted, and further updates are possible in the future.
 NOTE: Plain HTTP (port 80) is excluded after some consideration, since most of the request are based on HTTPS 
 """
-common_filter = 'not (port 53 or port 22 or port 3389 or port 5355 or port 5353 or port 3702 or port 123 or port 1900 or port 853 or port 80) and (tcp or udp)'
+common_filter = 'not (port 53 or port 22 or port 3389 or port 5355 or port 5353 or port 3702 or port 123 or port 1900 or port 853 or port 80 or port 8088) and (tcp or udp)'
 
 def capture(url, iface, output_file, timeout=200, capture_filter=common_filter, ill_files=None, log_output=None):
     stop_event = multiprocessing.Event()
